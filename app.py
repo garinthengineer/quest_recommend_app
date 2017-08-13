f8921ee6ddeb168381275a9ee33c55caf9a53c0e
garinthengineer <ilya.pukhov@gmail.com>
2017-08-13 20:56:03 +0300

    '123'

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def index():
 def result(output, url, req):
     return render_template('result.html', output=output, url=url, req=req)
 
- if __name__ == '__main__':
-     app.debug = True
-     port = int(os.environ.get("PORT", 5000))
-     app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+    app.debug = True
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
