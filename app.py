584cf9a4a8cfc7c0d2caf600b05c11c45b424c8b
garinthengineer <ilya.pukhov@gmail.com>
2017-08-14 21:01:59 +0300

    added field validation

--- app.py
+++ app.py
@@ -1,30 +1,31 @@
 from flask import Flask, render_template, request
 from flask_wtf import Form
-from wtforms.fields import TextField, SubmitField
-from wtforms.validators import NumberRange
+from wtforms.fields import TextField, SubmitField, SelectField
+from wtforms.validators import NumberRange, InputRequired
 import pandas as pd
 import numpy as np
 import os
 
 app = Flask(__name__)
-#app.config['SECRET_KEY'] = 'secret!' #this is to protect from CSRS attacks the string must be a secret word
+app.config['SECRET_KEY'] = 'secret!' #this is to protect from CSRS attacks the string must be a secret word
 data = pd.read_csv('quests.csv', sep=";", encoding='cp1251')
 rated = data[data[u'Атмосфера']>0].values
+choices = rated[:,1].tolist()
 
 class TextInput(Form):
-    questname = TextField(u'Название квеста:', [InputRequired()])
-    number = TextField(u'Число квестов:', [InputRequired()])
+    questname = SelectField(label='Название квеста', choices=[(f, f) for f in choices])
+    number = TextField(u'Число квестов:')
     submit = SubmitField(u'Показать')
-
-    def validate_number(form):
-        if int(form.number.data) <= 0 or int(form.number.data) > 460:
-            raise ValidationError(u'Пожалуйста, введите число между 0 и 460')
 
 @app.route('/', methods=['GET','POST'])
 def index():
+    errmsg = ''
     form = TextInput()
     if request.method == 'POST':
         req = form.questname.data
+        if str(form.number.data).isdigit() == False or int(form.number.data) not in range(1,460):
+            errmsg = u'Ошбка, Число квестов должно быть 0 до 460'
+            return render_template('index.html', form=form, errmsg=errmsg)
         numb = int(form.number.data)
         rest =  rated[np.where(rated[:,1] != req)][:,0:10]
         model = rated[np.where(rated[:,1] == req)][0,0:10]
@@ -38,7 +39,7 @@ def index():
             url.append('http://questguild.ru/quests/'+
                         str(rest[dist_2.index(min(sorted(dist_2)[i:]))][3]))
         return result(output=output, url=url, req=req)
-    return render_template('index.html', form=form)
+    return render_template('index.html', form=form, errmsg=errmsg)
 
 @app.route('/result')
 def result(output, url, req):
